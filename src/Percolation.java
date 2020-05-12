f35e3e4783a64ef1c4feff7b70e9c90628e7193a
willcohen2000 <willcohen2000@gmail.com>
2020-05-12 12:28:58 -0400

    Initial commit

---
+++ src/Percolation.java
@@ -0,0 +1,37 @@
+public class Percolation {
+
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return true;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+
+}
